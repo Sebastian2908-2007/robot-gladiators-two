486d319cbef4baa7e0713237620fa8fc9e0c6ddb
Sebabin <sebastiancrespin279@gmail.com>
2021-10-27 20:31:26 -0600

    console.log practice

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! JavaScript is running!");
+// this crealtes a function called fight
+var playerName = window.prompt("What is your robots name?");
+console.log(playerName);
+console.log("this logs a string goood for leaving yourself a message.");
+console.log(10 + 10);
+console.log("our robots name is " + playerName);
+
+
+function fight() {
+    window.alert("the fight has begun!");
+    
+};
+//fight();
